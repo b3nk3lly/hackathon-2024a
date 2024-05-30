0c52a88f872a48f8b64dd99c3aa7e34c833a31cd
b3nk3lly <benjkelly98@gmail.com>
2024-05-30 14:55:40 -0300

    Sleep for random amount of time

--- demo-app/src/main/java/ca/ctrl_alt_defeat/hackathon/HelloController.java
+++ demo-app/src/main/java/ca/ctrl_alt_defeat/hackathon/HelloController.java
@@ -3,6 +3,8 @@ package ca.ctrl_alt_defeat.hackathon;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloController {
 
@@ -10,7 +12,12 @@ public class HelloController {
     public String hello() {
         // Simulate some processing time
         try {
-            Thread.sleep(100);
+            Random r = new Random();
+            int low = 100;
+            int high = 3000;
+            int sleepMs = r.nextInt(high-low) + low;
+
+            Thread.sleep(sleepMs);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
